e49dcec65d903f0fd315490eaf946ee720928173
zhanglg <zhanglg@partnerx.cn>
2017-08-22 19:14:46 +0800

    demo overwrite base class function

---
+++ chapter_9/inheritance_03.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Fish
+{
+private:
+    bool m_isfreshwater;
+
+public:
+    Fish(bool isfreshwater)
+    {
+        m_isfreshwater = isfreshwater;
+    }
+
+    void swim()
+    {
+        if (m_isfreshwater)
+            cout << "is a fresh water fish!!!" << endl;
+        else
+            cout << "is a sea fish!!!" << endl;
+    }
+};
+
+class Tuna : public Fish
+{
+public:
+    Tuna() : Fish(false)
+    {
+        cout << "Tuna constructor!" << endl;
+        //m_isfreshwater = false;
+    }
+
+    void swim()
+    {
+        cout << "this is Tuna's swim!!!" << endl;
+    } 
+};
+
+class Carp : public Fish
+{
+public:
+    Carp() : Fish(true)
+    {
+        cout << "Carp constructor!" << endl;
+        //m_isfreshwater = true;
+    }
+    
+    void swim()
+    {
+        cout << "this is Carp's swim!!!" << endl;
+    }     
+};
+
+int main()
+{
+    Tuna tuna;
+
+    tuna.swim();
+
+    Carp carp;
+
+    carp.swim();
+
+    // carp.m_isfreshwater = false;
+    // tuna.m_isfreshwater = true;
+
+    // cout << "carp ";
+    // carp.swim();
+
+    // cout << "tuna ";
+    // tuna.swim();
+
+    return 0;
+}
