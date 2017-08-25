8a3d5bd6a72d9d28002c552a4211ad0bd2525c77
zhanglg <zhanglg@partnerx.cn>
2017-08-25 14:14:24 +0800

    add chapter_11 demo01

---
+++ chapter_11/demo01.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Fish
+{
+private:
+
+public:
+    void swim()
+    {
+        cout << "this is Fish swim!!!" << endl;
+    }
+};
+
+class Tuna : public Fish
+{
+public:
+    void swim()
+    {
+        cout << "this is Tuna swim!!!" << endl;
+    }
+};
+
+void my_fish(Fish &i_fish)
+{
+    i_fish.swim();
+}
+
+int main()
+{
+    Tuna tuna;
+
+    tuna.swim();
+    my_fish(tuna);
+
+    return 0;
+}
