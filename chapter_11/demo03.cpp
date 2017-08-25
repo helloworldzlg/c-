428c2a5e9d590728910406382bfabbc453d7640e
zhanglg <zhanglg@partnerx.cn>
2017-08-25 15:17:27 +0800

    modify demo03

--- chapter_11/demo03.cpp
+++ chapter_11/demo03.cpp
@@ -21,8 +21,8 @@ public:
 
 int main()
 {
-    cout << "size of base_1 is: " << sizeof(base_1) << endl;
-    cout << "size of base_2 is: " << sizeof(base_2) << endl;
+    cout << "size of base_1 is: " << sizeof(base_1) << endl;  // = 8
+    cout << "size of base_2 is: " << sizeof(base_2) << endl;  // = 16  (void *)p = 8   (64bit OS system)
 
     return 0;
 }
